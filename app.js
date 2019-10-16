7bdd43b9c5c0e5107363e8f63c422b569ed6d00f
Bahadir Balban <Bahadir Balban>
2019-10-16 10:14:28 -0700

    Update cors policy to include methods / credentials
    CORS AJAX from saasbox.net didn't work, let's try this update.

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-Auth-Token, Content-Type, Accept");
   next();
 });
