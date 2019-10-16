6600cb19d94269b963a6d0f5d32acbcfb1790c6b
Bahadir Balban <Bahadir Balban>
2019-10-16 12:19:55 -0700

    Add Authorization to the list of allowed headers

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true)
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-Auth-Token, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, 'Authorization', 'authorization', X-Auth-Token, Content-Type, Accept");
   next();
 });
 
