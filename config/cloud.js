d5de6eafc659fc18ef0ff9afbcafa5ecf0854896
Bahadir Balban <Bahadir Balban>
2019-11-05 17:31:13 -0800

    Adding config/cloud.js
    Sensitive fields in this file should refer to environment
    variables. The file itself is okay to put in version control.

---
+++ config/cloud.js
@@ -0,0 +1,12 @@
+
+
+
+exports.development = {
+	"jwt_secret" : "67c93071e063adead11bec4fde30110139c9",
+	"lambdaARN" : "arn:aws:iam::317925744813:role/service-role/ImageFix-role-yh8a5jhs"
+}
+
+exports.production = {
+	"jwt_secret" : "67c93071e063adead11bec4fde30110139c9",
+	"lambdaARN" : "arn:aws:iam::317925744813:role/service-role/ImageFix-role-yh8a5jhs"
+}
