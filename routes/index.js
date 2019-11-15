96e196d52a53bbfd7f21c66efb68e7d3cea4dac3
Bahadir Balban <Bahadir Balban>
2019-11-15 13:59:30 -0800

    Added uninstall setup route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ let service = require("../controllers/service");
 
 router.get("/query-setup-state", setup.query_setup_state)
 router.post("/submit-setup", setup.submit_setup)
+router.post("/uninstall-setup", setup.uninstall_setup)
 router.get("/fetch-optimization-records", service.fetch_optimization_records)
 /* GET home page. */
 router.get('/', function(req, res, next) {
