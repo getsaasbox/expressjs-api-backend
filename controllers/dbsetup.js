01f7f9e975f83bef48bf92b17e053d1020d8dc75
Team SaaSBox <team@saasbox.net>
2023-05-10 18:32:44 -0700

    add dbsetup file

---
+++ controllers/dbsetup.js
@@ -0,0 +1,16 @@
+
+
+/* This API is used to set up the SaaS */
+const env = process.env.NODE_ENV || "development";
+
+const admin = require("firebase-admin");
+
+// Cloud firestore key file.
+let serviceAccount = require(config.firestoreSecretPath);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+exports.db = db;
