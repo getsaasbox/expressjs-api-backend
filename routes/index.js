6fdb7ad6874649f54fd1023ecd52a986cca42a29
Team SaaSBox <team@saasbox.net>
2023-05-10 18:41:04 -0700

    Fix dbsetup.js reference in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-let setup = require("../controllers/setup");
+let setup = require("../controllers/dbsetup");
 let service = require("../controllers/service");
 
 router.post("/create-get-user", cluvio.create_get_user_info);
